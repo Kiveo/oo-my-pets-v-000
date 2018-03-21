dab49b07784ff5796861605121c3cf214d0e8645
Maverick Tesini <mavericktesini@gmail.com>
2018-03-21 19:25:43 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -42,7 +42,14 @@ class Owner
 
   def walk_dogs
     @pets[:dogs].each {|dog| dog.mood = "happy"}
-    # dog_list.each {|dog| dog.mood = "happy"}
+  end
+
+  def play_with_cats
+    @pets[:cats].each {|cat| cat.mood = "happy"}
+  end
+
+  def feed_fish
+    @pets[:fishes].each {|fish| fish.mood = "happy"}
   end
 
 end
