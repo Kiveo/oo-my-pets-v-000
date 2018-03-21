d2036042fdd3b05cf98db3093b993d09fe5e660f
Maverick Tesini <mavericktesini@gmail.com>
2018-03-21 18:39:28 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -2,9 +2,14 @@ class Owner
   attr_accessor :pets, :name
   attr_reader :species
 
+  @@all_owners = []
+  @@owner_count = 0
+
   def initialize(species)
     @species = species
     @pets = {:fishes => [], :dogs => [], :cats => [] }
+    @@owner_count += 1
+    @@all_owners << self
   end
 
   def say_species
