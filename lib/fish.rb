fb55888d45bf6adbddd20f8525253c8aeec2bd78
Maverick Tesini <mavericktesini@gmail.com>
2018-03-21 19:06:41 +0000

    automatically backed up by learn

--- lib/fish.rb
+++ lib/fish.rb
@@ -1,10 +1,13 @@
 class Fish
-  attr_reader :name
-  attr_accessor :mood
+  attr_reader :name, :mood
 
   def initialize(name)
     @name = name
     @mood = nervous
   end
 
+  def mood=(mood)
+    @mood = mood
+  end
+  
 end
