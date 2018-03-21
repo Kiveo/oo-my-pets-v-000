5b0d918a7b5a34ba7ac881a5af36229b8295ddfe
Maverick Tesini <mavericktesini@gmail.com>
2018-03-21 18:20:56 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,8 +1,13 @@
 class Owner
-  attr_accessor :owner_name, :pets
+  attr_accessor :owner_name, :species, :pets
 
   def initialize(owner_name)
     @owner_name = owner_name
+    @species = human
+  end
+
+  def say_species
+    puts species
   end
 
 end
